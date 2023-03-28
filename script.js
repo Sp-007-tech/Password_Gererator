1d9215ab2eafc710b28a945f66cc9d1c458ef0ca
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2023-03-28 12:46:35 +0530

    Updated !

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-function getPassword(){
+function getPassword()
+{
     var char = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOOPQRSTUVWXYZ!@#$%^&*()_+{}[]";
 
     var passLength = 8;
     var password = "";
 
-    for (var i=0; i<passLength; i++){
+    for (var i=0; i<passLength; i++)
+    {
         var randomNumber = Math.floor(Math.random() * char.length);
 
         password += char.substring(randomNumber,randomNumber+1);
     }
+    
     document.getElementById("password").value =password;
 }
